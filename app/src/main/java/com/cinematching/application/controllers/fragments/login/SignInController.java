dd5c70e2c48e61a53467489e693962f076b10581
Jean-Christophe MELIKIAN <jean.melikian@gmail.com>
2017-09-06 12:52:37 +0200

    Date picker

---
+++ app/src/main/java/com/cinematching/application/controllers/fragments/login/SignInController.java
@@ -0,0 +1,10 @@
+package com.cinematching.application.controllers.fragments.login;
+
+/**
+ * Created by Jean-Christophe Melikian on 06/09/2017.
+ */
+
+public interface SignInController {
+
+    public void authenticate();
+}
